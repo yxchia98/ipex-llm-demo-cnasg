50e4ba629932cc3913b5ad4a77a7b4975ec93859
yxchia <yxchia@localhost.localdomain>
2024-07-10 09:45:23 -0400

    added WIP llamaindex

--- llamaindex-rag/chroma.py
+++ llamaindex-rag/chroma.py
@@ -1,33 +1,11 @@
 # import
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.core import StorageContext, Settings, PromptTemplate, VectorStoreIndex, SimpleDirectoryReader
-from llama_index.llms.ipex_llm import IpexLLM
-from llama_index.embeddings.ipex_llm import IpexLLMEmbedding
+from llama_index.core import StorageContext, Settings, PromptTemplate
 import chromadb
 
 def completion_to_prompt(completion):
-    return f"<|system|>\n<|end|>\n<|user|>\n{completion}<|end|>\n<|assistant|>\n"
-
-
-# Transform a list of chat messages into zephyr-specific input
-def messages_to_prompt(messages):
-    prompt = ""
-    for message in messages:
-        if message.role == "system":
-            prompt += f"<|system|>\n{message.content}<|end|>\n"
-        elif message.role == "user":
-            prompt += f"<|user|>\n{message.content}<|end|>\n"
-        elif message.role == "assistant":
-            prompt += f"<|assistant|>\n{message.content}\n"
-
-    # ensure we start with a system prompt, insert blank if needed
-    if not prompt.startswith("<|system|>\n"):
-        prompt = "<|system|>\n<|end|>\n" + prompt
-
-    # add final assistant prompt
-    prompt = prompt + "<|assistant|>\n"
-
-    return prompt
+    return f"<|user|>\n{completion}\n<|assistant|>\n"
 
 
 SYSTEM_PROMPT = """You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
@@ -42,18 +20,16 @@ query_wrapper_prompt = PromptTemplate(
     "<|system|>\n" + SYSTEM_PROMPT + "\n<|user|>\n{query_str}\n<|assistant|>"
 )
 
-hf_model_path = "/llm-models/hf-models/Phi-3-mini-4k-instruct"
-saved_lowbit_model_path = "/llm-models/ipex-models/Phi-3-mini-4k-instruct"
-
 llm = IpexLLM.from_model_id_low_bit(
     model_name=saved_lowbit_model_path,
     tokenizer_name=hf_model_path,
     # tokenizer_name=saved_lowbit_model_path,  # copy the tokenizers to saved path if you want to use it this way
     context_window=4096,
     max_new_tokens=2048,
+    # completion_to_prompt=completion_to_prompt,
+    # messages_to_prompt=messages_to_prompt,
+    query_wrapper_prompt=query_wrapper_prompt,
     generate_kwargs={"temperature": 0.0, "do_sample": False},
-    completion_to_prompt=completion_to_prompt,
-    messages_to_prompt=messages_to_prompt,
 
 )
 
@@ -62,13 +38,24 @@ embed_model = IpexLLMEmbedding(model_name="/home/yxchia/llm-models/hf-models/bge
 Settings.llm = llm
 Settings.embed_model = embed_model
 
+# create client and a new collection
+chroma_client = chromadb.EphemeralClient()
+chroma_collection = chroma_client.create_collection("quickstart")
+
+# define embedding function
+embed_model = IpexLLMEmbedding(model_name="/home/yxchia/llm-models/hf-models/bge-large-en-v1.5", trust_remote_code=True)
+
 # load documents
 documents = SimpleDirectoryReader("./data/paul_graham/").load_data()
 
-index = VectorStoreIndex.from_documents(documents)
+# set up ChromaVectorStore and load in data
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+index = VectorStoreIndex.from_documents(
+    documents, storage_context=storage_context, embed_model=embed_model
+)
 
+# Query Data
 query_engine = index.as_query_engine()
-
 response = query_engine.query("What did the author do growing up?")
-
-print(response)
+display(Markdown(f"{response}"))
--- llamaindex-rag/ipex-llm.py
+++ llamaindex-rag/ipex-llm.py
@@ -4,7 +4,7 @@ from llama_index.llms.ipex_llm import IpexLLM
 
 # Transform a string into input zephyr-specific input
 def completion_to_prompt(completion):
-    return f"<|user|>\n{completion}\n<|assistant|>\n"
+    return f"<|system|>\n<|end|>\n<|user|>\n{completion}<|end|>\n<|assistant|>\n"
 
 
 # Transform a list of chat messages into zephyr-specific input
@@ -12,15 +12,15 @@ def messages_to_prompt(messages):
     prompt = ""
     for message in messages:
         if message.role == "system":
-            prompt += f"<|system|>\n{message.content}\n"
+            prompt += f"<|system|>\n{message.content}<|end|>\n"
         elif message.role == "user":
-            prompt += f"<|user|>\n{message.content}\n"
+            prompt += f"<|user|>\n{message.content}<|end|>\n"
         elif message.role == "assistant":
             prompt += f"<|assistant|>\n{message.content}\n"
 
     # ensure we start with a system prompt, insert blank if needed
     if not prompt.startswith("<|system|>\n"):
-        prompt = "<|system|>\n\n" + prompt
+        prompt = "<|system|>\n<|end|>\n" + prompt
 
     # add final assistant prompt
     prompt = prompt + "<|assistant|>\n"
@@ -36,8 +36,8 @@ warnings.filterwarnings(
 
 
 # llm = IpexLLM.from_model_id(
-#     model_name="/home/yxchia/llm-models/hf-models/Phi-3-mini-4k-instruct",
-#     tokenizer_name="/home/yxchia/llm-models/hf-models/Phi-3-mini-4k-instruct",
+#     model_name="/llm-models/hf-models/Phi-3-mini-4k-instruct",
+#     tokenizer_name="/llm-models/hf-models/Phi-3-mini-4k-instruct",
 #     context_window=512,
 #     max_new_tokens=128,
 #     generate_kwargs={"do_sample": False},
@@ -46,19 +46,20 @@ warnings.filterwarnings(
 # )
 
 
-hf_model_path = "/home/yxchia/llm-models/hf-models/Phi-3-mini-4k-instruct"
+hf_model_path = "/llm-models/hf-models/Phi-3-mini-4k-instruct"
 
-saved_lowbit_model_path = "/home/yxchia/llm-models/ipex-models/Phi-3-mini-4k-instruct"
+saved_lowbit_model_path = "/llm-models/ipex-models/Phi-3-mini-4k-instruct"
 
 llm = IpexLLM.from_model_id_low_bit(
     model_name=saved_lowbit_model_path,
     tokenizer_name=hf_model_path,
     # tokenizer_name=saved_lowbit_model_path,  # copy the tokenizers to saved path if you want to use it this way
-    context_window=2048,
+    context_window=4096,
     max_new_tokens=2048,
+    generate_kwargs={"temperature": 0.0, "do_sample": False},
     completion_to_prompt=completion_to_prompt,
-    generate_kwargs={"do_sample": False},
-    # messages_to_prompt=messages_to_prompt,
+    messages_to_prompt=messages_to_prompt,
+
 )
 
 
@@ -67,10 +68,10 @@ llm = IpexLLM.from_model_id_low_bit(
 # print(completion_response.text)
 
 
-# Streaming Text Completion
-response_iter = llm.stream_complete("Once upon a time, there's a little girl")
-for response in response_iter:
-    print(response.delta, end="", flush=True)
+# # Streaming Text Completion
+# response_iter = llm.stream_complete("What is AI?")
+# for response in response_iter:
+#     print(response.delta, end="", flush=True)
 
 
 # # Chat
@@ -79,9 +80,9 @@ for response in response_iter:
 # resp = llm.chat([message])
 # print(resp)
 
-# # Streaming Chat
-# from llama_index.core.llms import ChatMessage
-# message = ChatMessage(role="user", content="Once upon a time, there's a little girl")
-# resp = llm.stream_chat([message], max_tokens=1024)
-# for r in resp:
-#     print(r.delta, end="")
+# Streaming Chat
+from llama_index.core.llms import ChatMessage
+message = ChatMessage(role="user", content="What is AI?")
+resp = llm.stream_chat([message], max_tokens=256)
+for r in resp:
+    print(r.delta, end="")
